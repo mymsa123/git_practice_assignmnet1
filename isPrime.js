e3af59c1678332dabe3b52ebe821ba7c5614823a
mymsa123 <sabir70779560@gmail.com>
2023-01-18 22:27:25 +0530

    I have done it

---
+++ isPrime.js
@@ -0,0 +1,19 @@
+let n =13
+ let flag=true
+   for (let i = 2 ; i<num ; i--){
+  
+    if(num%i==0)
+    {
+        flag=false
+        break;
+    }
+   }
+   if(flag)
+   {
+       console.log("Yes")
+   }
+   else
+   {
+       console.log("No")
+   }
+   
